2da58eee23951325e00a64026c33712a807eec75
Diego Iván M.E <diegoivan.mae@gmail.com>
2025-02-27 10:42:24 -0600

    otsu: Crear detector Otsu
    Utiliza el método Otsu para el thresholding de la imagen.
    El problema con este método es que parece tener falsos positivos
    severos, como detectar grandes porciones de la arena como detecciones.

---
+++ latas_template_matching/DetectorTemplate.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class DetectorTemplate:
+    def obtener_detecciones(self, imagen):
+        """
+        Obtiene las detecciones de latas para la imagen dada
+        :param imagen:
+        :return:
+        """
